eef804f60d3c5c4a7dde5a76344c55e1e5a6eb39
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-02-16 15:21:07 -0800

    ALX C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-puts(""Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+int main()
+{
+	char source_file[] = "CFILE";
+        char executable_file[] = ""Programming is like building a multilingual puzzle";	
+	
+	if (system("gcc -Wall -Werror -Wextra -pedantic -std=gnu89 CFILE  -o "Programming is like building a multilingual puzzle") == 0)
+	{	
+		write("Compilation successful\n");
+		return (0);
+	}
+
+}
