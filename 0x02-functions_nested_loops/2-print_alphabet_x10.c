4132e70447b42efb3ced2b679667649bae58cf11
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-02-21 18:35:33 -0800

    Heyaa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,46 +1,20 @@
 #include "main.h"
-
-
 /**
-
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-
- * followed by a new line
-
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
+ * Return: On success 1.
  */
-
 void print_alphabet_x10(void)
-
 {
+	char i, k;
 
-        char ch;
+	for (k = 0; k < 10; k++)
+	{
 
-        int i;
-
-
-        i = 0;
-
-
-        while (i < 10)
-
-        {
-
-                ch = 'a';
-
-                while (ch <= 'z')
-
-                {
-
-                        _putchar(ch);
-
-                        ch++;
-
-                }
-
-                _putchar('\n');
-
-                i++;
-
-        }
-
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
+
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,28 +1,20 @@
 #include "main.h"
 
-
 /**
-
- * _abs - computes the absolute value of an integer
-
- * @n: the int to check
-
- * Return: the absolute value of int
-
+ *_abs-prints the absolute value of an integer.
+ *@a:the integer to compute its absolute value.
+ *Return:returns the abs value.
+ *
  */
-
-int _abs(int n)
-
+int _abs(int a)
 {
+	int k = a;
+	int absvalue;
 
-        if (n >= 0)
-
-        {
-
-                return (n);
-
-        }
-
-        return (-n);
-
+	if (k < 0)
+	{
+		k = k * (-1);
+	}
+	absvalue = k;
+	return (absvalue);
 }
