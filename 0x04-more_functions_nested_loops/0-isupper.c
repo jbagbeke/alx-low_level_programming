54c50703f361339552cae18d9c74d2ed1ee84333
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-02-23 17:05:22 -0800

    Heyaa

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,4 @@
 #include "main.h"
-
-/**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
- */
-
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -32,3 +16,4 @@ int main(void)
     printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
+
