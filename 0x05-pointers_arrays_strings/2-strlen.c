1cb0f9fa4091b4f7efec431da65604b5d8996682
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-02-27 15:20:28 -0800

    Heyaa

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _strlen - Hehehe
+ * @s: input which is a character
+ */
+
+int _strlen(char *s)
+{
+	printf("The value of 'string *s' is: %lu\n", *s);
+}
