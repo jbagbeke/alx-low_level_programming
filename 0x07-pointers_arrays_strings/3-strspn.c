4dd2d4174aed154c33ed85f78b2488f88adcee4d
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-06 03:23:53 -0800

    Heyaa

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strspn - length of prefix
+ *Return: Always 0 (Success)
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	char *s = accept;
+	int len = strspn (*s, 6);
+	printf("The length of the prefix substring is: %d\n", s);
+	return (0);
+}
