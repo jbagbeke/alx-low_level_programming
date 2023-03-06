b96ccc213a2a9c0bb99a4826c4be4f1c406f3217
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-06 04:15:52 -0800

    Heyaa

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,11 +11,8 @@ unsigned int _strspn(char *s, char *accept)
 	char *p = accept;
 	int count = 0;
 
-	while (*s != '\0')
 	{
-	{
-		while (*s && strchr(accept, *s++));
-	}
+	while (*s && strchr(accept, *s++));
 	count++;
 	}
 
