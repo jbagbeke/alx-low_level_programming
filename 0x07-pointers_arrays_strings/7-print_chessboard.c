53f6511927e2ede05b577cd039c785878ab88855
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-06 17:54:41 -0800

    Heyaa

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,16 +5,22 @@
  * @a: array
  * Return: Always 0 (Success)
  */
-void print_chessboard(char (*a)[8]) {
-    int i, j;
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            if ((i + j) % 2 == 0) {
-                printf(" ");
-            } else {
-                printf("#");
-            }
-        }
-        printf("\n");
-    }
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
+{	
+	if ((i + j) % 2 == 0)
+{
+	printf(" ");
+}	else
+{
+	printf("#");
+}
+}
+	printf("\n");
+}
+	return (0);
 }
