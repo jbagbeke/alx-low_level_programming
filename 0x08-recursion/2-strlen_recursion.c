b46d688c11f5d72c1e673ee0ad7208a3a12228c3
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-08 17:03:16 -0800

    Heyaa

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - String lenght of s
+ * Return: n
+ *@s: string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = _strlen_recursion(s);
+	printf("The length of string is %d\n", n);
+	return (n);
+}
