c1136f462e91387a057e9a319ebe7630e5a26464
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-08 16:47:30 -0800

    Heyaa

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,16 +3,16 @@
  * _print_rev_recursion - reverses a string
  * @s: the string to be reversed
  * Return: void
+ *main - Entry point
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 	else
 		_putchar('\n');
-
 }
