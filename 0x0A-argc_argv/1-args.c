57a97e729fbd3043a2f2b0f710f67511d002f549
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-10 18:09:32 -0800

    Heyaa

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: counter
+ *@argv: Argument vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
