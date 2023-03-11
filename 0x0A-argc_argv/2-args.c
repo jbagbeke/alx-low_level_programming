d500277eb383c3161a87caf2a86e6d679c7b3da6
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-10 18:23:03 -0800

    Heyaa

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *@argc: Counter
+ *@argv: Vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	for (i = 0; i < argc; i++)
+	{	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
