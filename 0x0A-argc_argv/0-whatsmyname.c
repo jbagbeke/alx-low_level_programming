f3d93cf198572b8cfe93c4eefe6ae23b99821dc3
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-10 17:06:04 -0800

    Heyaa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *@argc: Argument counter
+ *@argv: Argument vector
+ *
+ *Return: 0 (Success)
+ */
+int main(int argc, *argv[])
+{
+	printf("The file name is: %s", argv[0]);
+	for (i = 0; i < argc; i++)
+	{
+	printf("The file name is: %s", argv[i]);
+	}
+	return (0);
+}
