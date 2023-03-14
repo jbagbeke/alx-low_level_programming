4b7c83dff05c7170d391a9fb85c77e6a39f214fb
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-14 16:26:42 -0700

    Heyaa

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,60 @@
-#include "main.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
- *create_array - Function
- *@size: Size of memory to be allocated
- *@c: Char initialise
- *Return: Void
+ * create_array - Prototype
+ * @size: size of array
+ * @c: character to be initialised to
+ * Return: bee (success), NULL (Fail)
  */
+
 char *create_array(unsigned int size, char c)
 {
-	char park[5] = {'c'};
 	char *bee;
 
-	bee = (*char)malloc(size * sizeof(char));
-	
-	if (size == 0)
+	bee = malloc(size * sizeof(char));
+
+	for (int i = 0; i < size; i++)
 	{
-		return (NULL);
+	bee[i] = c;
+	}
+
+	if (bee == NULL)
+	{
+	printf("Memory allocation failed");
+	return (NULL);
+	}
+	return (bee);
+}
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	unsigned int size;
+	char c;
+
+	printf("Enter the size of array: ");
+	scanf("%u", &size);
+	printf("Enter char you want to initialise to: ");
+	scanf(" %c", &c);
+
+	char *bee = create_array(size, c);
+
+	if (bee != NULL)
+	{
+	printf("The array of size %u is initialised to %c", size, c);
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%c", bee[i]);
 	}
 
 	free(bee);
+	}
+
+	return (0);
 }
