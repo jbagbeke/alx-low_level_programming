220cff9354f27cac738d180786991b55896fea47
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-14 17:53:33 -0700

    Heyaa

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	int size;
+	char *free[];
+
+	str = malloc(size * sizeof(*free));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	free(str);
+	return (str);
+}
