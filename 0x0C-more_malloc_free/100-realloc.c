20ed8c12ccb0b1bfac6bb8f8c529dc81dfe44f75
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-03-17 16:40:38 -0700

    Heyaa

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,1 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *_realloc - Function that reallocates memory
+ *@ptr: Points to old_size
+ *@old_size: Old memory allocated
+ *@new_size: New memory to be allocated
+ *Return: Pointer point
+ *
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+        char *point;
+
+        ptr = malloc(old_size);
+        point = malloc(new_size);
+
+        if (new_size > old_size)
+        {
+                for (int i = 0; i <= old_size; i++)
+                {
+                        point[i] = ptr[i];
+                }
+        }
+        if (new_size < old_size)
+        {
+                for (int i = 0; i <= new_size; i++)
+                {
+                        point[i] = ptr[i];
+                }
+        }
+        if (new_size == old_size)
+        {
+                return (ptr);
+        }
+
+        if (ptr = NULL)                                                                                                         
+        {
+                return (malloc(new_size));
+        }
+        if (new_size = NULL || ptr != NULL)
+        {
+                free(ptr);
+        }
+
+        return (point);
+        free(point);
+}
 
