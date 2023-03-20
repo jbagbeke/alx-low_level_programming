affd97e610bc22cbfb11042fb973e7c54020d026
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-20 15:35:00 -0700

    Heyaa

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,11 +5,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-struct *dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+struct dog(char *name, float age, char *owner);
+/**
+ * dog - a function that will be used in my task code
+ * @name: Name to be entered
+ * @age: age of user
+ * @owner: The ownwer info
+ */
 
 #endif
