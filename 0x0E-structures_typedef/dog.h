c525a8df797285e58a8466f5da4ae42f04667d86
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-20 16:30:02 -0700

    Heyaa

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,10 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**my_dog - typedef for struct dog
+ */
 typedef struct dog my_dog;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
