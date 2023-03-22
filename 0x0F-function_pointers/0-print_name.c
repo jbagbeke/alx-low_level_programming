4d414a1a309db52de69d02278ed338bdc3f1aac9
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-22 16:26:44 -0700

    Heyaa

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,51 +2,18 @@
 #include "function_pointers.h"
 
 /**
- * print_name_like_that - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_like_that(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 
-/**
- * print_name_upper - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_upper(char *name)
+void print_name(char *name, void (*f)(char *))
 {
-    unsigned int i;
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            _putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            _putchar(name[i]);
-        }
-        i++;
-    }
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_name("Henry", print_name_like_that);
-    print_name("Henry Hasford", print_name_upper);
-    printf("\n");
-    return (0);
+	f(name);
 }
