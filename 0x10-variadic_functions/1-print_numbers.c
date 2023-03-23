50492371aa73882b867ae383f97c5055a7f67c62
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-23 14:09:23 -0700

    Heyaa

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    unsigned int i;
+    int x;
+    va_list args;
+
+    va_start(args, n);
+
+
+    for (i = 0; i < n; i++)
+    {
+        x = va_arg(args, int);
+        printf("&d", x);
+    }
+
+    if (i != (n - 1) && separator != NULL)
+    {
+        printf("%s", separator);
+    }
+
+        _putchar('\n');
+
+        va_end(args);
+}
