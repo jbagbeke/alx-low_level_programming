7a273cd47fa3b2b305ec52141ce47a83402dce7b
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-23 13:29:39 -0700

    Heyaa

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,21 +9,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int sum = 0;
-    int x;
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+	int x;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+	return (0);
 
-    for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 {
-    x = va_arg(args, int);
-    sum += x;
+	x = va_arg(args, int);
+	sum += x;
 }
-   va_end(args);
-    return sum;
+	va_end(args);
+	return (sum);
 }
