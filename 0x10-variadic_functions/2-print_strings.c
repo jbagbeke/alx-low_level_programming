fdbb19c42762fdf5eedb3a205ef2677ccff7299f
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-23 15:26:11 -0700

    Heyaa

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,9 +23,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
         if (i != (n - 1) && separator != NULL)
             printf("%s", separator);
 
-        printf("\n");
+        /*printf("\n");*/
 
         /*va_end(args);*/
     }
+    printf("\n");
+
     va_end(args);
 }
