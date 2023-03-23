a464921de2ecb1fa11de6c048d40fa7361dc3613
Joshua Bubune <joshua.legithive@gmail.com>
2023-03-23 15:16:12 -0700

    Heyaa

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    unsigned int i;
+    va_list args;
+    char *hey;
+
+    va_start(args, n);
+
+    for (i = 0; i < n; i++)
+    {
+        hey = va_arg(args, char *);
+
+        if (hey == NULL)
+        {
+            printf("(nil)");
+        }
+        else
+        {
+            printf("%s", hey);
+        }
+
+        
+        if (i != (n - 1) && separator != NULL)
+        {
+            printf("%s", separator);
+        }
+
+        printf("\n");
+
+        va_end(args);
+    }
+}
