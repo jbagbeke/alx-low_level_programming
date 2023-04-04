85e718fa93a5e224a3f4cebb506cc0692ca2a66f
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-04 16:11:40 -0700

    Heyaa

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *listint_len - Function to return number of elements
+ *@h: Header node
+ *Return: Number of elements (num)
+ *
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		num++;
+	}
+
+	return (num);
+}
