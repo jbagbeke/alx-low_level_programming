f76249a729272206d7f67dd9430464ca2bd75ee2
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-04 15:59:35 -0700

    Heyaa

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,14 +12,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->data);
+		printf("%d\n", h->n);
 		h = h->next;
-		n++;
+		num++;
 	}
 
-	return (n);
+	return (num);
 }
