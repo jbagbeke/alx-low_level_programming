d4d343b62adf5873e3827e71807e27d5b03a6de2
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-07 17:31:42 -0700

    Heyaa

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  *
  *
@@ -8,36 +9,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int hoo;
+	int hoo;
+	char *h;
+	ssize_t reading;
+	ssize_t writing;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
+	if (filename == NULL)
+	{
+	    return (0);
+	}
 
-    hoo = open(filename, O_RDONLY);
-    if (hoo == -1)
-    {
-        //close(hoo);
-        return (0);
-    }
+	hoo = open(filename, O_RDONLY);
+	if (hoo == -1)
+	{
+	return (0);
+	}
 
-    char hey[letters];
+	h = malloc(letters * sizeof(char));
+	reading = read(hoo, h, letters);
+	if (reading == -1)
+	{
+		close(hoo);
+		free(h);
+		return (0);
+	}
+	
+	writing = write(STDOUT_FILENO, h, reading);
+	if (writing == -1 || writing != reading)
+	{
+	close(hoo);
+	free(h);
+	return (0);
+	}
 
-    ssize_t reading = read(hoo, hey, letters);
-    if (reading == -1)
-    {
-        close(hoo);
-        return (0);
-    }
-
-    ssize_t writing = write(STDOUT_FILENO, hey, reading);
-    if (writing == -1 || writing != reading)
-    {
-        close(hoo);
-        return (0);
-    }
-
-    close(hoo);
-    return (writing);
+	close(hoo);
+	free(h);
+	return (writing);
 }
