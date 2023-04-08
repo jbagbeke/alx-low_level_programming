66226a85280abe88199b154e68d0946d5bb8a2f0
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-08 15:52:59 -0700

    Heyaa

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#define BUFF 1024
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int copyfile_from_to(const char *file_from, const char *file_to)
+{
+    int fd_val;
+    int fd_value;
+    int hoo, reading, writing;
+    char buffer[BUFF];
+
+    if (file_from == NULL || file_to == NULL)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    fd_val = open(file_from, O_RDONLY | O_CREAT | O_EXCL, 0664);
+    if (fd_val == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", "file_from");
+        exit(98);
+    }
+
+    fd_value = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0664);
+    if (fd_value == -1)
+    {
+        printf(STDERR_FILENO, "Error: Can't write to %d\n", "file_to");
+        exit(99);
+    }
+
+    while (reading = read(fd_val, buffer, BUFF) > 0)
+    {
+        writing = write(fd_value, buffer, reading);
+        if (reading == -1)
+        {
+            exit(98);
+        }
+        if (writing = -1)
+        {
+            exit(99);
+        }
+        if (close(fd_val == -1))
+        {
+            dprintf(STDERR_FILENO, "Error: Can't close fd %d", "fd_val");
+            exit(100);
+        }
+        if(close(fd_value == -1))
+        {
+            dprintf(STDERR_FILENO, "Error: Can't close fd %d", "fd_value");
+            exit(100);
+        }
+    }
+
+    close(fd_val);
+    close(fd_value);
+    return (writing);
+}
