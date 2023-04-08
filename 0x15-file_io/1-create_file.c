7f4f3788b703681c1f2661611259576716e0acaf
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-07 19:51:59 -0700

    Heyaa

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	hoo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	hoo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (hoo == -1)
 	{
 		close(hoo);
