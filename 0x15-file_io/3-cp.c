9980fdae3e43d3877777c5ff6548575d7875163e
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-09 04:49:56 -0700

    Heyaa

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,8 +15,20 @@ void closing(int fd)
 	hoo = close(fd);
 	if (hoo == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d", hoo);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", hoo);
 	exit(100);
+	}
+}
+
+void buff(char *filename)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]0);
+		exit(99);
 	}
 }
 
@@ -27,7 +39,7 @@ int main(int argc, char *argv[])
     int fd_val;
     int fd_value;
     int reading, writing;
-    char buffer[BUFFER_SIZE];
+    char *buffer;
 
     if (argc != 3)
     {
@@ -39,24 +51,29 @@ int main(int argc, char *argv[])
 
     fd_value = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0664);
 
+    buffer = buff(arg[2]);
     while ((reading = read(fd_val, buffer, BUFFER_SIZE)) > 0)
     {
         writing = write(fd_value, buffer, reading);
         if (reading == -1 || fd_val == -1)
         {
             dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	    free(buffer);
 	    exit(98);
         }
         if (writing == -1 || fd_value == -1)
         {
             dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	    free(buffer);
 	    exit(99);
       }
-        closing(fd_val);
+        free(buffer);
+	closing(fd_val);
 	closing(fd_value);
 	fd_value = open(argv[2], O_WRONLY | O_APPEND);
 }
 
+	free(buffer);
 	closing(fd_val);
 	closing(fd_value);
 	return (0);
