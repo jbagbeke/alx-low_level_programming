468fb9c3d0df0281f1f2b486fd555fc2ba2305a3
Jay_jay <joshua.legithive@gmail.com>
2023-04-09 14:30:58 -0700

    Heyaa

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 
 	reading = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_WRONLY | O_APPEND);
-	} while (reading > 0);
+	} while ((reading = read(file_from, buffer, 1024)) > 0);
 
 	free(buffer);
 	closing(file_from);
