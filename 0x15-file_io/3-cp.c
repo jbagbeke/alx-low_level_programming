323c6f3cc6fcf91fd52b9a7605d65b8060c746f9
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-04-09 13:00:32 -0700

    Heyaa

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     }
 
     file_from = open(argv[1], O_RDONLY);
-    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
     buffer = malloc(1024 * sizeof(char));
     if (buffer == NULL)
