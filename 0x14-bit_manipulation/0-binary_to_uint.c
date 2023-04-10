53a6d77625bab783b59ec2d0a83a58dfb38ad080
Jay_jay <joshua.legithive@gmail.com>
2023-04-10 16:05:18 -0700

    Heyaa

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 
     for (i = strlen(b) - 1; i >= 0; i--)
     {
-        if (b[i] != '0' & b[i] != '1')
+        if (b[i] != '0' && b[i] != '1')
         {
             return (0);
         }
