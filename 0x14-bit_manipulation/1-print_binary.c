464b2e6ac1a093511d65d8ea453c84710aeade16
Jay_jay <joshua.legithive@gmail.com>
2023-04-10 19:32:12 -0700

    Heyaa

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_binary - Function to convert decimal to binary
+ *@n: number to be converted to binary
+ *Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	int hey = 0;
+	unsigned long int hoo;
+
+	for (i = 50; i >= 0; i--)
+	{
+	hoo = n >> i;
+
+	if (hoo && 1)
+	{
+	_putchar('1');
+	hey++;
+	}
+	else if (hey)
+	_putchar('0');
+	}
+	if (!hey)
+	_putchar('0');
+}
