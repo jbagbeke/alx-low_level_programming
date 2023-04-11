91c407cddaac920d47e8d4cac40fcac50b66b174
Jay_jay <joshua.legithive@gmail.com>
2023-04-10 17:17:53 -0700

    Heyaa

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,32 @@
 #include "main.h"
 #include <math.h>
 /**
- *
- *
- *
+ *binary_to_uint - Function to convert bin to dec
+ *@b: Pointer to string of binary numbers
+ *Return - Result of function (num)
  *
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
-    int i;
-    int hoo;
+	unsigned int num = 0;
+	int i;
+	int hoo;
 
-    if (b == NULL)
-    {
-    	return (0);
-    }
+	if (b == NULL)
+	{
+	return (0);
+	}
 
-    for (i = strlen(b) - 1; i >= 0; i--)
-    {
-        if (b[i] != '0' && b[i] != '1')
-        {
-            return (0);
-        }
+	for (i = strlen(b) - 1; i >= 0; i--)
+	{
+	if (b[i] != '0' && b[i] != '1')
+	{
+	return (0);
+	}
 
-        hoo = b[i] - '0';
-        num += hoo;
+	hoo = b[i] - '0';
+	num += hoo;
 
-    }
-
-    return (num);
-
+	}
+	return (num);
 }
