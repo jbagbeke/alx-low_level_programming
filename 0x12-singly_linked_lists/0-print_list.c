c5040ff170de845f9a741f15857469066b39ffc5
Jay_jay <joshua.legithive@gmail.com>
2023-04-16 06:55:13 -0700

    Heyaa

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,23 +8,21 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n = 1;
-	char *str;
-
-	str = malloc(sizeof(list_t));
+	size_t n = 0;
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		n++;
-
-		if (str == NULL)
-		{
-			printf("[0] (nil)");
-		}
 	}
 
-	free(str);
 	return (n);
 }
