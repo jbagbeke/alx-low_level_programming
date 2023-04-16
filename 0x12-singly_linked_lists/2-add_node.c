bbadc3733ec0b72eb4d95870dac1a6bd42787b80
Jay_jay <joshua.legithive@gmail.com>
2023-04-16 12:43:50 -0700

    Heyaa

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_list;
 
 	new_list = malloc(sizeof(list_t));
-	if (new_list == NULL)
+	if (new_list == NULL || str == NULL)
 	{
 		return (NULL);
 	}
