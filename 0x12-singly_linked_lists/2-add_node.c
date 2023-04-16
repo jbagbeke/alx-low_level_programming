c79f8ed28c5e824bb36b09d8c41e0848025a378f
Jay_jay <joshua.legithive@gmail.com>
2023-04-16 13:04:47 -0700

    Heyaa

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,10 +17,6 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_list->str = strdup(str);
-	if (new_list->str == NULL)
-	{
-		return (NULL);
-	}
 
 	new_list->len = strlen(str);
 	new_list->next = *head;
