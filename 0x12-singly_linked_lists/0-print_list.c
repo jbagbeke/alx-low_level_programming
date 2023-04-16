fbacdbcbb20e9c43bbd46dd36097d5989e2d51c3
Jay_jay <joshua.legithive@gmail.com>
2023-04-16 05:55:05 -0700

    Heyaa

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,8 @@
 size_t print_list(const list_t *h)
 {
 	size_t n = 1;
-	list_t *list1;
-	
-	list1 = malloc(sizeof(list_t));
-	h = list1;
 
-	if (list1->str == NULL)
+	if (h->str == NULL)
 	{
 		printf("[0] (nil)\n");
 	}
@@ -25,7 +21,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		n++;
 	}
-
-	free(list1);
 	return (n);
 }
