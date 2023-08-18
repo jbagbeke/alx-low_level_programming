1c1d5e87db9ee48348fbe8b136040eb9764553f4
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-18 18:50:42 +0000

    Double

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	ptr1 = *head;
 	if (ptr1 == NULL)
 		return (-1);
-
 	if (index == 0)
 	{
 		if (ptr1->next != NULL)
@@ -23,24 +22,18 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			*head = ptr2;
 		}
 		else
-		{
 			*head = NULL;
-		}
-
 		free (ptr1);
 		return (1);
 	}
-
 	while (index >= 1)
 	{
 		ptr2 = ptr1;
 		ptr1 = ptr1->next;
-
 		if (!ptr1)
 			return (-1);
 		index--;
 	}
-
 	if (ptr1->next == NULL)
 		ptr2->next = NULL;
 	else
@@ -49,7 +42,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		ptr2->next = ptr3;
 		ptr3->prev = ptr2;
 	}
-
 	free (ptr1);
 	return (1);
 }
