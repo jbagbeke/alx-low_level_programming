1dc581976a3be6d52fa19e530107ec96ef8abad5
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-18 16:54:50 +0000

    Double

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - dels node at index
+ * @head: Pointer to first node of d-list
+ * @index: index of node to delete
+ * Return: 1(Success), 0 otherwise
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *ptr1, *ptr2, *ptr3;
+
+	ptr1 = *head;
+	if (ptr1 == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		if (ptr1->next != NULL)
+		{
+			ptr2 = ptr1->next;
+			ptr2->prev = NULL;
+			*head = ptr2;
+		}
+		free (ptr1);
+		return (1);
+	}
+
+	while (index >= 1)
+	{
+		ptr2 = ptr1;
+		ptr1 = ptr1->next;
+
+		if (!ptr1)
+			return (-1);
+		index--;
+	}
+
+	if (ptr1->next == NULL)
+		ptr2->next = NULL;
+
+	if (ptr1->next != NULL)
+	{
+		ptr3 = ptr1->next;
+		ptr2->next = ptr3;
+		ptr3->prev = ptr2;
+	}
+
+	free (ptr1);
+	return (1);
+}
