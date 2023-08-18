734792e884002f8ed2326c6569d40f19dcba92dc
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-18 15:02:52 +0000

    Double

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node1;
+	dlistint_t *node1, *ptr1, *ptr2;
 
 	node1 = malloc(sizeof(dlistint_t));
 
@@ -16,16 +16,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node1->prev = NULL;
 	node1->next = NULL;
 
-	if (*h == NULL)
-		return (*h);
-
+	ptr1 = *h;
+	if (ptr1 == NULL || idx == 0)
+	{
+		node1->next = ptr1;
+		*h = node1;
+		return (node1);
+	}
 
 	while (idx >= 1)
 	{
-		*h = (*h)->next;
-		if(*h == NULL)
+		ptr1 = ptr1->next;
+
+		if (ptr1 == NULL)
 			return (NULL);
 		idx--;
 	}
-	return (*h);
+	ptr2 = ptr1->next;
+
+	ptr1->next = node1;
+	node1->prev = ptr1;
+	node1->next = ptr2;
+	ptr2->prev = node1;
+
+	return (node1);
 }
