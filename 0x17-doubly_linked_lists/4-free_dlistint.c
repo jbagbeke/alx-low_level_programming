ec2054ca24a228adaceb0e11d25960ecdbc199c5
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-18 03:08:21 +0000

    Double

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees nodes of a d-list
+ * @head: Pointer to first node
+ * Return: Void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	if (head == NULL)
+		exit(0);
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
