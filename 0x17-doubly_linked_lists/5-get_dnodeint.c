c1632e820dfb1fbe3784a5a09b9707161b128a5e
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-18 03:33:59 +0000

    Double

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets node at index
+ * @head: Points to first node of d-list
+ * @index: Index of node to return
+ * Return: Pointer to node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (index == 0 || head == NULL)
+		return (head);
+
+	while (index != 1)
+	{
+		head = head->next;
+
+		if (head == NULL)
+			return (NULL);
+
+		index--;
+	}
+
+	return (head);
+}
