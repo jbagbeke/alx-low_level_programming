4e368d30c49b7b65eb94c8baaeef48320dda9819
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2023-08-22 02:21:07 +0000

    Dynamic

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <unistd.h>
 /**
- *_putchar - Function
- *@c: character C
- *Return: 0
+ *_putchar - Function to return char to stdout
+ *Return: char c
+ *@c: Char c to be returned to stdout
+ *
  */
 int _putchar(char c)
 {
-	_putchar(c);
-	return (0);
+	return (write(1, &c, 1));
 }
