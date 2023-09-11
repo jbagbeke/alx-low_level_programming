bcbc25d05cbbf586650001df42f81911e8085cf1
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-10 18:10:09 -0700

    ALX_AND_ME

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table with struct defined in header
+ * @size: Size of hash_table to be created
+ * Return: The hash table that was created
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_tab;
+	int i;
+
+	if (!(size > 0))
+	{
+		fprintf(stderr, "Invalid Size of Hash Table\n");
+		exit(EXIT_FAILURE);
+	}
+
+	hash_tab = malloc(sizeof(hash_table_t));
+
+	if (!hash_tab)
+		return NULL;
+
+	hash_tab->size = size;
+
+	hash_tab->array = malloc(size * sizeof(hash_node_t *));
+
+	if (!hash_tab)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		hash_tab->array[i] = NULL;
+
+	return (hash_tab);
+}
