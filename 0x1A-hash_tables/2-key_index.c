0939c93827a6fd7d87c831131611d0fda138b7fd
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-11 04:52:13 -0700

    ALX_AND_ME

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,27 +9,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_table_t *hash_tab;
 	unsigned long int index;
-
-	if (!(size > 0))
-	{
-		fprintf(stderr, "Invalid Size of Hash Table");
-		return (-1);
-	}
-
-	hash_tab = malloc(sizeof(hash_table_t));
-
-	if (!hash_tab)
-		return (-1);
-
-	hash_tab->size = size;
-	hash_tab->array = malloc(size * sizeof(hash_node_t *));
-
-	if (!hash_tab->array)
-		return (-1);
-
-	index = hash_djb2(key);
+	
+	index = hash_djb2(key) % size;
 
 	return (index);
 }
