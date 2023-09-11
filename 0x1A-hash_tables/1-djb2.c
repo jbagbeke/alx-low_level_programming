9a884af4a2de91a05ceae275d5786fd67c3624de
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-10 18:47:08 -0700

    ALX_AND_ME

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash_it;
+	int b;
+
+	hash_it = 5381;
+	while ((b = *str++))
+	{
+		hash_it = ((hash_it << 5) + hash_it) + b;
+	}
+
+	return (hash_it);
+}
---
+++ 0x1A-hash_tables/t.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+int main()
+{
+	char *str = "Hello World";
+
+	while (*str++)
+	{
+		printf("%c\n", *str);
+	}
+
+	return (0);
+}
