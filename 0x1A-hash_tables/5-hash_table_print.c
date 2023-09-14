1d758e6c6696c3482b1ecef6410d6235e5a9b5b9
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-13 19:50:22 -0700

    ALX_AND_ME

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,32 +13,24 @@ void hash_table_print(const hash_table_t *ht)
 	char *hash_key, *hash_val;
 	hash_node_t *print_it;
 
-	if (!ht)
-	{
-		printf("{}\n");
+	if (ht == NULL)
 		return;
-	}
 
 	printf("{");
-	
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] == NULL)
 			continue;
-
 		hash_key = strdup(ht->array[i]->key);
 		hash_val = strdup(ht->array[i]->value);
-
 
 		if (!hash_val || !hash_key)
 		{
 			printf("}\n");
 			break;
 		}
-
 		if (ind)
 			printf(", ");
-
 		if (ht->array[i]->next == NULL)
 		{
 			printf("'%s': '%s'", hash_key, hash_val);
@@ -50,11 +42,9 @@ void hash_table_print(const hash_table_t *ht)
 				print_it = ht->array[i];
 				printf("'%s': '%s'", hash_key, hash_val);
 				print_it = print_it->next;
-
 				if (print_it != NULL)
 					printf(", ");
 			} while (print_it != NULL);
-
 		}
 	}
 	printf("}\n");
