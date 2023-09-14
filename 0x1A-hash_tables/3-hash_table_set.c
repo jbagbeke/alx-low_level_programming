5e809070e0de1309119397d62622e020dca2fc3e
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-13 17:21:51 -0700

    ALX_AND_ME

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hash_node;
 	char *value_copied, *key_copied;
 
-	if (strlen(key) == 0)
+	if (ht == NULL || key == NULL ||strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -25,17 +25,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	key_copied = strdup(key);
 
 	if (!value_copied || !key_copied)
+	{
+		free(hash_node);
 		return (0);
+	}
 
 	hash_node->key = key_copied;
 	hash_node->value = value_copied;
 	hash_node->next = NULL;
 
 	if (ht->array[index] == NULL)
+	{
 		ht->array[index] = hash_node;
-
+		return (1);
+	}
 	if (ht->array[index] != NULL)
 	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			ht->array[index]->value = value_copied;
+			return (1)
+		}
 		hash_node->next = ht->array[index];
 		ht->array[index] = hash_node;
 	}
