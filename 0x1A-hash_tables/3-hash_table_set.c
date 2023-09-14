b54075dbcd41dddc1352b592ceb805e397fd1d26
Joshua Bubune <joshua.legithive@gmail.com>
2023-09-13 17:24:42 -0700

    ALX_AND_ME

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hash_node;
 	char *value_copied, *key_copied;
 
-	if (ht == NULL || key == NULL ||strlen(key) == 0)
+	if (strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
